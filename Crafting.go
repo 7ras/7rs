4bb8206cbe2bc78e03b500cf5398d457df855bdb
7RS <178391874+7ras@users.noreply.github.com>
2024-08-14 22:48:18 +0300

    Update Crafting.go

--- Crafting.go
+++ Crafting.go
@@ -1,15 +1,44 @@
 package crafting
 
 // Reverse reverses the input string.
-func Mix(Items1 string, Items2 string) string {
+func Grid(Items1 string, Items2 string,Items3 string, Items4 string) string {
 
-	Mixer := Items1 +" "+ Items2
-	
+	TOP := Items1 +","+ Items2
+	BOT := Items1 +","+ Items2
+
+	if TOP == "," {
+		TOP = ""
+	} else if BOT == "," {
+		BOT = ""
+	}
+
+	Mixer:= TOP + BOT 
+
+	if Mixer == "Cobblestone,Cobblestone,Cobblestone,Cobblestone" {
+		Mixer = "bigblock"
+		}
 
 	return Mixer
 
 
 }
+
+func Hopper(Items1 string, filter string)cut string, but string {
+	cut := ""
+	but := ""
+	for_,char := range Items1 {
+		if char == filter {
+		  cut = char+ ","
+		}
+		if char != filter {
+		  but = char+ ","
+		}
+		
+	}
+	
+	return cut , but
+}
+
 
 func Bucket(Items string)string {
 	if Items == "Water"{
